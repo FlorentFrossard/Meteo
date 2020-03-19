72c908fef95e2d2e3f864d0531106aaf2cd6167c
Florent Frossard <florentwm@gmail.com>
2020-03-19 10:21:44 +0100

    Initial Commit

---
+++ MisterMeteo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MisterMeteo
+//
+//  Created by Florent Frossard on 19/03/2020.
+//  Copyright © 2020 Florent Frossard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
